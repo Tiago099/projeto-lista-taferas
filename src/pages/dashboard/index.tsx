8beec7be24252222147a1a01cfbb7dc0c42a6b0a
Tiago Sebastian <tiagosebastian99@gmail.com>
2023-03-14 18:01:56 -0300

    Criando dashboard pt2

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -2,6 +2,8 @@ import styles from "./styles.module.css";
 import Head from "next/head";
 import { GetServerSideProps } from "next";
 import { Textarea } from "../../components/textarea";
+import { FiShare2 } from "react-icons/fi";
+import { FaTrash} from "react-icons/fa";
 
 import { getSession } from "next-auth/react";
 
@@ -28,10 +30,31 @@ export default function Dashboard(){
                     </form>
                  </div>
                </section>
+
+               <section className={ styles.taskContainer}>
+                <h1>Minhas tarefas</h1>
+
+                <article className={styles.task}>
+                    <div className={styles.tagContainer}>
+                        <label className={styles.tag}>PUBLICO</label>
+                        <button className={styles.shareButton}>
+                            <FiShare2 size={22} color="#3183ff"/>
+                        </button>
+                    </div>
+
+                    <div className={styles.taskContent}>
+                        <p>Minha primeira tarefa de exemplo show demais!</p>
+                        <button className={styles.trashButton}>
+                            <FaTrash size={24} color="#ea3140"/>
+                        </button>
+                    </div>
+                </article>
+               </section>
             </main>    
         </div>
     )
 }
+
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) =>{
     const session = await getSession({ req });
